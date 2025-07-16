af78ab61dfc7c08e4db2d1cbaf17da9e6afae8b2
Sebastian Rakel <sebastian@devunit.eu>
2025-07-16 13:04:30 +0200

    Install da-one themes

--- scripts/flake-module.nix
+++ scripts/flake-module.nix
@@ -1,12 +1,11 @@
-{ lib, ... }:
-{
+{ lib, ... }: {
   perSystem = { pkgs, ... }:
     let
       scheme_path = "./schemes";
       template_path = "./templates";
       theme_path = "./themes";
       cache_path = "./cache";
-      
+
       schemes = [
         "github"
         "nord"
@@ -14,8 +13,10 @@
         "penumbra"
         "harmonic"
         "modus"
+        "da-one-black"
+        "da-one-white"
       ];
-      
+
       templates = [
         "alacritty"
         "emacs"
@@ -30,31 +31,27 @@
         "halloy"
       ];
 
-      python = pkgs.python3.withPackages (ps: with ps; [
-        requests
-        pystache
-        pyyaml
-      ]);
-      
-      base16-builder = pkgs.runCommandLocal "base16-builder"
-        {
-          nativeBuildInputs = [
-            pkgs.makeWrapper
-            python
-          ];
-          script = ./base16-builder.py;
-        }''
-         makeWrapper $script $out/bin/$name \
-                     --prefix PATH : ${lib.makeBinPath [ python ]} \
-                     --add-flags "--scheme_path ${scheme_path}" \
-                     --add-flags "--template_path ${template_path}" \
-                     --add-flags "--themes_path ${theme_path}" \
-                     --add-flags "--cache_path ${cache_path}" \
-                     --add-flags "--schemes ${builtins.concatStringsSep "," schemes}" \
-                     --add-flags "--templates ${builtins.concatStringsSep "," templates}"
-        '';
-    in
-    {
+      python =
+        pkgs.python3.withPackages (ps: with ps; [ requests pystache pyyaml ]);
+
+      base16-builder = pkgs.runCommandLocal "base16-builder" {
+        nativeBuildInputs = [ pkgs.makeWrapper python ];
+        script = ./base16-builder.py;
+      } ''
+        makeWrapper $script $out/bin/$name \
+                    --prefix PATH : ${lib.makeBinPath [ python ]} \
+                    --add-flags "--scheme_path ${scheme_path}" \
+                    --add-flags "--template_path ${template_path}" \
+                    --add-flags "--themes_path ${theme_path}" \
+                    --add-flags "--cache_path ${cache_path}" \
+                    --add-flags "--schemes ${
+                      builtins.concatStringsSep "," schemes
+                    }" \
+                    --add-flags "--templates ${
+                      builtins.concatStringsSep "," templates
+                    }"
+      '';
+    in {
       apps.base16-builder.program = "${base16-builder}/bin/base16-builder";
     };
 }
